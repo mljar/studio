8dd7d06e3df7b021cf2acf56f7b572d15c96522c
Piotr <pplonski86@gmail.com>
2024-11-28 12:37:31 +0100

    change installation order for jupyter ai

--- src/main/utils.ts
+++ src/main/utils.ts
@@ -362,8 +362,8 @@ export async function installCondaPackEnvironment(
       | { type: 'pip'; package: string };
 
     const installSteps: InstallStep[] = [
-      { type: 'wheel', file: 'jupyter_ai-0.6.0-py3-none-any.whl' },
       { type: 'wheel', file: 'jupyter_ai_magics-0.6.0-py3-none-any.whl' },
+      { type: 'wheel', file: 'jupyter_ai-0.6.0-py3-none-any.whl' },
       { type: 'wheel', file: 'pieceofcode-0.6.0-py3-none-any.whl' },
       { type: 'pip', package: 'dask[dataframe]' },
       { type: 'pip', package: '-U numpy==1.26.4' }
